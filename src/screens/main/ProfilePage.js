204f6da5e7aa5b1d206964dd3c11c29370aa3a94
NTIVUGURUZWA Emmanuel <62064898+devemmm@users.noreply.github.com>
2021-10-16 02:50:02 +0200

    profile updates

--- src/screens/main/ProfilePage.js
+++ src/screens/main/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useContext, useEffect } from 'react'
 import { View, Text, ScrollView, Image, TouchableOpacity, Alert, StyleSheet, Platform, TextInput } from 'react-native'
-import { bright, H, StatusBarHeight, _grey } from '../../constants/constants'
+import { APP_BACKGROUND_COLOR, APP_ORANGE_COLOR, APP_WHITE_COLOR, bright, H, StatusBarHeight, _grey } from '../../constants/constants'
 import { BottomSheet } from 'react-native-btr';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import serverConfig from '../../constants/server.json';
@@ -23,78 +23,18 @@ const ProfilePage  = ({navigation})=>{
     const [edit, setEdit] = useState(false)
     const [username, setUsername] = useState('Loading...')
     const [token, setToken] = useState('')
-    // constructor(props) {
-    //     super(props)
     
-    //      = {
-    //         about: false,
-    //         terms: false,
-    //         edit: false,
-    //         username: 'Loading...',
-    //         token: ''
-    //     }
-    // }
-
-    // componentDidMount() {
-    //     this.FetchUser();
-    // }
-
-    // FetchUser = async() => {
-    //     try {
-    //         await AsyncStorage.getItem('@USERDATA').then((userNewData) => {
-    //             const user = userNewData ? JSON.parse(userNewData) : [];
-    //             this.setState({ username: user.owner, token: user.token });
-    //         });
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-
-    // signout = async(token) => {
-    //     try {
-    //         this.setState({ submitting: true});
-
-    //         const value = await fetch(`${serverConfig.restServer}/user/signout`, {
-    //         method: "POST",
-    //         headers: {
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json'
-    //         },
-    //         'Authorization': 'Bearer ' + token            
-    //         });
-    //         const response = await value.json();
-    //         if (response.statusCode == 200 ) {
-    //             Alert.alert(
-    //                 'Log out',
-    //                 'You are about to log out, are you sure?',
-    //                 [
-    //                     { text: 'Cancel', onPress: () => { return null } },
-    //                     {
-    //                         text: 'Confirm', onPress: () => {
-    //                             AsyncStorage.remove('@USERDATA');
-    //                             this.props.navigation.navigate('SignupScreen');
-    //                         }
-    //                     },
-    //                 ],
-    //                 { cancelable: false }
-    //             )
-    //         }
-            
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
-    
+    const [names, setNames] = useState('')
     return (
-        <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1, marginTop: StatusBarHeight, backgroundColor: 'white' }}>
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
             <View style={{ paddingHorizontal: 15, height: H*.1,justifyContent: 'center' }}>
-                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Profile</Text>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: APP_WHITE_COLOR }}>Profile</Text>
             </View>
 
             <View style={{ paddingVertical: 10, paddingHorizontal: 15, flexDirection: 'row', alignItems: 'center' }}>
                 <Image source={{ uri: "https://yocast-api.nextreflexe.com/images/background/yocast.jpeg" }} style={{ width: 80, height: 80, borderRadius: 50, resizeMode: 'cover' }} />
                 <View style={{ marginLeft: 20, flex: 1 }}>
-                    <Text numberOfLines={1} style={{ fontSize: 16, marginBottom: 10, fontWeight: 'bold', textTransform: 'capitalize' }}>{state.user.email}</Text>
+                    <Text numberOfLines={1} style={{ fontSize: 16, marginBottom: 10, fontWeight: 'bold', textTransform: 'capitalize', color: APP_WHITE_COLOR }}>{state.user.email}</Text>
                     <View style={{ flexDirection: 'row' }}>
                         <Text style={{ backgroundColor: '#000', color: bright, paddingHorizontal: 10, paddingVertical: 2, borderRadius: 3, fontSize: 12 }}>Premium</Text>
                     </View>
@@ -157,23 +97,23 @@ const ProfilePage  = ({navigation})=>{
 
             <BottomSheet
             visible={about}>
-                <View style={{ flex: 1, backgroundColor: 'white', paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
+                <View style={{ flex: 1, backgroundColor: APP_BACKGROUND_COLOR, paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
                 <ScrollView>
             <View style={{ height: 60, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 15 }}>
                 <TouchableOpacity 
                 onPress={() => setAbout(false)} 
                 style={{ flex: .25, paddingVertical: 5, paddingRight: 10 }}>
-                    <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28 }} />
+                    <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28, tintColor: APP_WHITE_COLOR }} />
                 </TouchableOpacity>
                 
                 <View style={{ flex: .25 }}/>
             </View>
             <View style={{ paddingHorizontal: 15, paddingTop: H * .03, paddingBottom: 20, width: '80%' }}>
-                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Hi, We are YoCast</Text>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: APP_ORANGE_COLOR}}>Hi, We are YoCast</Text>
             </View>
 
             <View style={{ marginHorizontal: 15 }}>
-            <Text>Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
+            <Text style={{color: APP_WHITE_COLOR}}>Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
 
 Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
 Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
@@ -194,23 +134,23 @@ Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage pro
 
             <BottomSheet
             visible={terms}>
-                <View style={{ flex: 1, backgroundColor: 'white', paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
+                <View style={{ flex: 1, backgroundColor: APP_BACKGROUND_COLOR, paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
                 <ScrollView>
             <View style={{ height: 60, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 15 }}>
                 <TouchableOpacity 
                 onPress={() => setterms(false)} 
                 style={{ flex: .25, paddingVertical: 5, paddingRight: 10 }}>
-                    <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28 }} />
+                    <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28, tintColor: APP_WHITE_COLOR}} />
                 </TouchableOpacity>
                 
                 <View style={{ flex: .25 }}/>
             </View>
             <View style={{ paddingHorizontal: 15, paddingTop: H * .03, paddingBottom: 20, width: '80%' }}>
-                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Read our terms carefuly</Text>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: APP_ORANGE_COLOR }}>Read our terms carefuly</Text>
             </View>
 
             <View style={{ marginHorizontal: 15 }}>
-            <Text>Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
+            <Text style={{color: APP_WHITE_COLOR}}>Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
 
 Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
 Like your lorem ipsum extra crispy? Then Bacon Ipsum is the placeholder text generator for you. Side of eggs and hashbrowns is optional, but recommended.
@@ -230,22 +170,22 @@ Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage pro
 
 
             <BottomSheet visible={edit}>
-                <View style={{ flex: 1, backgroundColor: '#fff', paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
+                <View style={{ flex: 1, backgroundColor: APP_BACKGROUND_COLOR, paddingTop: Platform.OS === 'ios' ? StatusBarHeight : null }}>
                 <ScrollView >
                     <View style={{ height: 60, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 15 }}>
                         <TouchableOpacity onPress={() => setEdit(false)} style={{ flex: .25, paddingVertical: 5, paddingRight: 10 }}>
-                            <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28 }} />
+                            <Image source={require('../../../assets/arrow-left.png')} style={{ height: 28, width: 28, tintColor: APP_WHITE_COLOR }} />
                         </TouchableOpacity>
-                        <Text style={{flex: .5, textAlign: 'center', fontSize: 17, fontWeight: 'bold' }}>Edit Profile</Text>
+                        <Text style={{flex: .5, textAlign: 'center', fontSize: 17, fontWeight: 'bold', color: APP_WHITE_COLOR }}>Edit Profile</Text>
                         <TouchableOpacity style={{ flex: .25, paddingVertical: 5 }}>
-                            <Text style={{ color: _grey, textAlign: 'right', fontSize: 17 }}>Save</Text>
+                            <Text style={{ color: APP_ORANGE_COLOR, textAlign: 'right', fontSize: 17 }}>Save</Text>
                         </TouchableOpacity>
                     </View>
 
                     <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: H * .03 }}>
                     <Image source={{ uri: "https://images.unsplash.com/photo-1530785602389-07594beb8b73?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHwxMnx8YmxhY2slMjBmYWNlfGVufDB8fHx8MTYyODEwMDQ1Nw&ixlib=rb-1.2.1&q=80&w=1080" }} style={{ width: 120, height: 120, borderRadius: 70, resizeMode: 'cover' }} />
                     <TouchableOpacity style={{ paddingVertical: 5, marginTop: 5 }}>
-                            <Text style={{ textAlign: 'right', fontSize: 16 }}>Change Photo</Text>
+                            <Text style={{ textAlign: 'right', fontSize: 16, color: APP_WHITE_COLOR }}>Change Photo</Text>
                         </TouchableOpacity>
                     </View>
 
@@ -259,13 +199,14 @@ Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage pro
                         marginVertical: 30
                     }}>
                         <TextInput
-                        value="Emmy Emmy"
-                        // autoFocus
-                        placeholder="Username"
-                        style={{
-                            flex: 1,
-                            fontSize: 15
-                        }} />
+                            style={{ flex: 1, fontSize: 15}} 
+                            value={names}
+                            // autoFocus
+                            placeholder="Username"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            onChangeText={(names)=>setNames(names)}
+                        />
                     </View>
                 </ScrollView>
                 </View>
@@ -277,14 +218,19 @@ Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage pro
 
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1, 
+        backgroundColor: APP_BACKGROUND_COLOR
+    },
     profile_set: {
         borderBottomColor: '#ebebeb',
-        borderBottomWidth: 1,
+        borderBottomWidth: 0.2,
         paddingVertical: 10
     },
     haeder: {
         fontSize: 15,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        color: APP_WHITE_COLOR
     },
     det: {
         fontSize: 11,
--- src/screens/subscription/Subscription.js
+++ src/screens/subscription/Subscription.js
@@ -1,40 +1,292 @@
-import React, { Component } from 'react'
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
-import {  H, StatusBarHeight, _grey } from '../../constants/constants'
-
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import { APP_BACKGROUND_COLOR, APP_ORANGE_COLOR, APP_WHITE_COLOR, StatusBarHeight, _grey } from '../../constants/constants';
 
 export class Subscription extends Component {
-    render() {
-        return (
-        <ScrollView style={{ marginTop: StatusBarHeight }}>
-            <View style={{ height: 60, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 15 }}>
-                <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{ flex: .25, paddingVertical: 5, paddingRight: 10 }}>
-                    <Image source={require('../../../assets/arrow-left.png')} style={{ height: 30, width: 30 }} />
-                </TouchableOpacity>
-                <Text style={{flex: .5, textAlign: 'center', fontSize: 17, fontWeight: 'bold' }}>Subscriptions</Text>
-                <View style={{ flex: .25 }}/>
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showPackageDetails: false,
+      package: 'standard',
+      //  premium, standard, basic,
+      loading: true
+    };
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: APP_BACKGROUND_COLOR,
+        }}>
+        <View
+          style={{
+            height: 60,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingHorizontal: 15,
+          }}>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.goBack()}
+            style={{ flex: 0.25, paddingVertical: 5 }}>
+            <Image
+              source={require('../../../assets/arrow-left.png')}
+              style={{ height: 30, width: 30, tintColor: APP_WHITE_COLOR}}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              flex: 0.5,
+              textAlign: 'center',
+              fontSize: 20,
+              fontWeight: 'bold',
+              color: APP_ORANGE_COLOR
+            }}>
+            Choose a Plan
+          </Text>
+          <View style={{ flex: 0.25 }} />
+        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{ paddingHorizontal: 15, paddingBottom: 20, width: '80%' }}>
+            <Text style={{ fontSize: 13, color: APP_ORANGE_COLOR }}>
+              You can upgrade or downgrade anytime.
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => this.setState({ package: 'premium' })}
+            style={{
+              marginHorizontal: 15,
+              backgroundColor: 'grey',
+              borderColor: this.state.package === 'premium' ? APP_ORANGE_COLOR : '#ebebeb',
+              borderWidth: 3,
+              paddingVertical: 15,
+              paddingHorizontal: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 5,
+              }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 12, color: APP_ORANGE_COLOR }}>
+                Premium Package
+              </Text>
+              <Text style={{ fontWeight: 'bold', fontSize: 12, color: APP_WHITE_COLOR }}>
+                then $ 30.25/year
+              </Text>
             </View>
-            <View style={{ paddingHorizontal: 15, paddingTop: H * .03, paddingBottom: 20, width: '80%' }}>
-                <Text style={{ fontSize: 22, fontWeight: 'bold' }}>Choose a Plan</Text>
-                <Text style={{ fontSize: 13, color: _grey }}>Youn upgrade or downgrade anytime.</Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 10, color: APP_WHITE_COLOR }}>Save 63%</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: 10, color: APP_WHITE_COLOR }}>
+                12 months at $ 2.520/month
+              </Text>
             </View>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate("SubscriptionPay")}  style={{ marginHorizontal: 15, backgroundColor: 'orange', paddingVertical: 10, paddingHorizontal: 15, borderRadius: 7, marginBottom: 10 }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 15 }}>
-                    <Text style={{ fontWeight: 'bold', fontSize: 19 }}>Basic</Text>
-                    <Text style={{ fontWeight: 'bold', fontSize: 19 }}>RWF 9,900</Text>
-                </View>
-                <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Includes:</Text>
+            {this.state.package === 'premium' && (
+              <View style={{}}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 13,
+                    marginTop: 10,
+                    marginBottom: 5,
+                    color: APP_ORANGE_COLOR
+                  }}>
+                  What you get:
+                </Text>
                 <View>
-                    <Text>Unlimited podcasts</Text>
-                    <Text>Access to premium content</Text>
-                    <Text>Allow downloads</Text>
-                    <Text>No ads</Text>
-                    <Text>Get credits</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
                 </View>
-            </TouchableOpacity>
+              </View>
+            )}
+          </TouchableOpacity>
+
+          {/* recommended */}
+          <TouchableOpacity
+            onPress={() => this.setState({ package: 'standard' })}
+            style={{
+              marginHorizontal: 15,
+              backgroundColor: 'grey',
+              paddingVertical: 15,
+              paddingHorizontal: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+              borderColor: this.state.package === 'standard' ? APP_ORANGE_COLOR : '#ebebeb',
+              borderWidth: 3,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginBottom: 5,
+              }}>
+              <Text style={styles.subscription_name}>
+                Standard Package
+              </Text>
+              <Text style={styles.subscription_price}>
+                then $ 15.15/6_month
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={{ fontWeight: 'bold', fontSize: 10, color: APP_WHITE_COLOR }}>Save 72%</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: 10, color: APP_WHITE_COLOR }}>
+                6 months at $ 2.525/month
+              </Text>
+            </View>
+
+            {this.state.package === 'standard' && (
+              <View style={{}}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 13,
+                    marginTop: 10,
+                    marginBottom: 5,
+                    color: APP_ORANGE_COLOR
+                  }}>
+                  What you get:
+                </Text>
+                <View>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+
+          {/* monthly */}
+          <TouchableOpacity
+            onPress={() => this.setState({ package: 'basic' })}
+            style={{
+              marginHorizontal: 15,
+              backgroundColor: 'grey',
+              borderColor: this.state.package === 'basic' ? APP_ORANGE_COLOR : '#ebebeb',
+              borderWidth: 3,
+              paddingVertical: 15,
+              paddingHorizontal: 10,
+              borderRadius: 5,
+              marginBottom: 10,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                paddingVertical: 5,
+              }}>
+              <Text style={styles.subscription_name}>Basic Package</Text>
+              <Text style={styles.subscription_price}>
+                $ 2.85/month
+              </Text>
+            </View>
+
+            {this.state.package === 'basic' && (
+              <View>
+                <Text
+                  style={styles.subscription_cont}>
+                  What you get:
+                </Text>
+                <View>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                  <Text style={styles.package_offer}>Unlimited podcasts</Text>
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
         </ScrollView>
-        )
-    }
+        <View style={{ marginHorizontal: 15, marginVertical: 10 }}>
+          <TouchableOpacity
+            // onPress={() => this.props.navigation.navigate("SubscriptionPay")}
+            style={{
+              backgroundColor: APP_ORANGE_COLOR,
+              height: 45,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 5,
+            }}>
+            <Text style={{ color: '#fff' }}>
+              {this.state.package === 'premium'
+                ? 'GET SPECIAL OFFER'
+                : this.state.package === 'standard'
+                ? 'PAY SUBSCRIPTION'
+                : this.state.package === 'basic'
+                ? 'START NOW'
+                : null}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
-export default Subscription
+
+const styles = StyleSheet.create({
+    subscription_name:{
+        fontWeight: 'bold', 
+        fontSize: 12,
+        color: APP_ORANGE_COLOR
+    },
+    subscription_price:{
+        fontWeight: 'bold', 
+        fontSize: 12,
+        color: APP_WHITE_COLOR
+    },
+    subscription_cont:{
+        fontWeight: 'bold',
+        fontSize: 13,
+        marginTop: 10,
+        marginBottom: 5,
+        color: APP_ORANGE_COLOR
+    },
+    package_offer:{
+        fontSize: 12,
+        color: APP_WHITE_COLOR
+    }
+})
+export default Subscription;
+
+/*
+
+4 packages
+-
+
+payment methods
+-
+
+===> flutter
+
+- modal home page ==> reminding remaining days
+7 days to go
+on click ==> subscription page
+
+
+*/
