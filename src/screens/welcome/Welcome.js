9b3bb7867cff20ac8a6d7f7dcc6197f22c308b71
devemmm <djntivuguruzwaemmanuel@gmail.com>
2021-11-02 00:38:21 +0200

    updating welcome screen

--- src/screens/welcome/Welcome.js
+++ src/screens/welcome/Welcome.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View, FlatList, ImageBackground } from 'react-native'
-import { MaterialCommunityIcons } from 'react-native-vector-icons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { bright, H, W, StatusBarHeight, content } from '../../constants/constants'
 
 
-const Welcome = ()=> {
+const Welcome = ({navigation})=> {
+
         return (
             <View style={styles.container}>
                 <FlatList
@@ -12,57 +13,43 @@ const Welcome = ()=> {
                     pagingEnabled
                     data={content}
                     keyExtractor={item => item.id}
-                    renderItem={({ item }) => {
+                    renderItem={({ index, item }) => {
                         return (
                             <ImageBackground 
                                 source={{ uri: item.image }}
                                 resizeMode = "cover"
-                                style={{
-                                    height: H,
-                                    width: W,
-                                }}
+                                style={{ height: H, width: W}}
                             >
                             <View style={{ position: 'absolute', height: H, width: W, backgroundColor: 'black', opacity: .5 }} />
-                                <View style={{
-                                    top: H * .6,
-                                    left: 0,
-                                    width: '80%',
-                                    paddingLeft: 25
-                                }}>
-                                    <Text style={{
-                                        color: bright,
-                                        fontSize: 22,
-                                        textTransform: 'capitalize', 
-                                        fontWeight: 'bold'
-                                    }}>{item.title}</Text>
-                                    <Text style={{ color: bright, fontSize: 12 }}>lorem asdas sda ssda asdas dsad sdasdas</Text>
+                                <View style={{ top: H * .6, left: 0, width: '80%', paddingLeft: 25}}>
+                                    <Text style={{ color: bright, fontSize: 22, textTransform: 'capitalize', fontWeight: 'bold'}}>{item.title}</Text>
+                                    <Text style={{ color: bright, fontSize: 12 }}>{item.description}</Text>
                                 </View>
 
                             
 
-                                { item.last === 0 ? 
-                                <MaterialCommunityIcons name="gesture-swipe-right" size={24} color="white" style={{ position:  'absolute', bottom: 50, right: 25 }} /> : 
-                                <TouchableOpacity 
-                                    onPress={() => this.props.navigation.navigate("WelcomeOptions") } 
-                                    style={{ position: 'absolute', bottom: 50, right: 0, left: 0, backgroundColor: '#fff', height: 45, borderRadius: 30, alignItems: 'center', justifyContent: 'center', marginHorizontal: 25 }}>
-                                    <Text>Get started</Text>
-                                </TouchableOpacity> }
+                                <View style={{top: H * .7, left: 0, width: '100%'}}>
+
+                                    { item.last === 0 ? 
+
+                                        <MaterialCommunityIcons name="gesture-two-double-tap" size={30} color="white" style = {{alignSelf: 'flex-end', marginRight: '10%'}} />
+                                        
+                                        :
+                                        <TouchableOpacity 
+                                            onPress={() => navigation.navigate("WelcomeOptions") } 
+                                            style={{ position: 'absolute', bottom: 10, right: 0, left: 0, backgroundColor: '#fff', height: 45, borderRadius: 30, alignItems: 'center', justifyContent: 'center', marginHorizontal: 25 }}>
+                                            <Text>Get started</Text>
+                                        </TouchableOpacity> 
+                                    }
+                                </View>
                             </ImageBackground>
                         )
                 }} />
             
                 <TouchableOpacity 
-                    onPress={() => this.props.navigation.navigate("WelcomeOptions") } 
-                    style={{
-                        position: 'absolute',
-                        right: 25, 
-                        top: StatusBarHeight + 25,
-                        backgroundColor: 'rgba(255, 255, 255, .5)',
-                        paddingHorizontal: 20,
-                        paddingVertical: 5,
-                        borderRadius: 30
-                    }}>
-                        <Text style={styles.skip_btntxt}>SKIP</Text>
+                    onPress={() => navigation.navigate("WelcomeOptions") } 
+                    style={styles.skip_btn}>
+                    <Text style={styles.skip_btntxt}>SKIP</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -78,9 +65,10 @@ const styles = StyleSheet.create({
         position: 'absolute',
         right: 25, 
         top: StatusBarHeight + 25,
-        // backgroundColor: 'rgba(255, 255, 255, .5)',
+        backgroundColor: 'rgba(255, 255, 255, .5)',
         paddingHorizontal: 20,
-        paddingVertical: 7,
+        paddingVertical: 5,
+        borderRadius: 30
     },
     skip_btntxt : {
         color: bright
--- src/screens/welcome/WelcomeOptions.js
+++ src/screens/welcome/WelcomeOptions.js
@@ -5,22 +5,22 @@ import { bright, dark, H, W, _grey } from '../../constants/constants'
 export class WelcomeOptions extends Component {
     render() {
         return (
-        <View style={{ flex: 1 }}>
-            <Image source={{ uri: 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw1fHxzdHVkaW98ZW58MHx8fHwxNjI4MDk2MDY1&ixlib=rb-1.2.1&q=80&w=1080'}} style={{ height: H *.55, width: W }} />
-            <View style={{ height: H * .45 + 15, backgroundColor: 'white', paddingHorizontal: 25, position: 'absolute', bottom: 0, right: 0, left: 0, borderTopEndRadius: 15, borderTopStartRadius: 15, paddingTop: H * .1 }}>
-                {/* <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', borderColor: '#ebebeb', borderWidth: 2, justifyContent: 'center', height: 50, borderRadius: 5, marginBottom: 15 }}>
-                    <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJjDBqPtCIXHk6p3jEvflNw-_2poZ-YFvFIw&usqp=CAU'}} style={{ height: 25, width: 25 }} />
-                    <Text style={{ color: _grey, marginLeft: 15, fontSize: 16 }}>Sign up with Google</Text>
-                </TouchableOpacity> */}
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("SignupScreen")} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 45, borderRadius: 30, marginBottom: 20, backgroundColor: dark }}>
-                    <Text style={{ color: bright, marginLeft: 15, fontSize: 16 }}>Sign up with Email</Text>
-                </TouchableOpacity>
+            <View style={{ flex: 1 }}>
+                <Image source={{ uri: 'https://images.unsplash.com/photo-1478737270239-2f02b77fc618?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTgwOTN8MHwxfHNlYXJjaHw1fHxzdHVkaW98ZW58MHx8fHwxNjI4MDk2MDY1&ixlib=rb-1.2.1&q=80&w=1080'}} style={{ height: H *.55, width: W }} />
+                <View style={{ height: H * .45 + 15, backgroundColor: 'white', paddingHorizontal: 25, position: 'absolute', bottom: 0, right: 0, left: 0, borderTopEndRadius: 15, borderTopStartRadius: 15, paddingTop: H * .1 }}>
+                    {/* <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', borderColor: '#ebebeb', borderWidth: 2, justifyContent: 'center', height: 50, borderRadius: 5, marginBottom: 15 }}>
+                        <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJjDBqPtCIXHk6p3jEvflNw-_2poZ-YFvFIw&usqp=CAU'}} style={{ height: 25, width: 25 }} />
+                        <Text style={{ color: _grey, marginLeft: 15, fontSize: 16 }}>Sign up with Google</Text>
+                    </TouchableOpacity> */}
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate("SignupScreen")} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 45, borderRadius: 30, marginBottom: 20, backgroundColor: dark }}>
+                        <Text style={{ color: bright, marginLeft: 15, fontSize: 16 }}>Sign up with Email</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => this.props.navigation.navigate("LoginScreen")} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 45, borderRadius: 30, marginBottom: 20, backgroundColor: "rgba(0,0,0,.1)" }}>
-                    <Text style={{ color: '#000', textAlign: 'center', fontSize: 15 }}>Login to your existing account</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate("LoginScreen")} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 45, borderRadius: 30, marginBottom: 20, backgroundColor: "rgba(0,0,0,.1)" }}>
+                        <Text style={{ color: '#000', textAlign: 'center', fontSize: 15 }}>Login to your existing account</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
         )
     }
 }
